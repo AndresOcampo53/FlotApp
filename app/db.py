d7c62e22e532018df942a5081c3e915458ddae66
BlackjetX <santiagoxrp@gmail.com>
2021-06-30 21:06:23 -0500

    Base de datos terminada

--- app/db.py
+++ app/db.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-engine = create_engine('postgres://postgres:localhost@localhost:5432/Universidad')
+engine = create_engine('postgresql://postgres:3636@localhost:5432/Universidad')
 Session = sessionmaker(bind=engine)
 session = Session()
 
--- app/db_models.py
+++ app/db_models.py
@@ -2,11 +2,10 @@ import enum
 import uuid
 
 from db import Base
-from flask_security import UserMixin, RoleMixin
 from sqlalchemy.orm import relationship, backref
 from sqlalchemy import Boolean, DateTime, Column, Integer, \
     String, ForeignKey, JSON, Float, TypeDecorator
-from sqlalchemy.dialects.postgresql import UUID
+
 
 """------Definicion de tablas------"""
 
@@ -18,7 +17,7 @@ class usuarios(Base):
     apellido_usuario = Column(String(30), nullable=False)
     cedula_usuario = Column(String(30), nullable=False, unique=True)
     rol_usuario = Column(String(20), nullable=False)
-    logsUsuarios = relationship("logsusuarios", back_populates="usuarios")
+    logsUsuarios = relationship("logs_usuarios", back_populates="usuarios")
 
     def __init__(self, nombre_usuario, apellido_usuario, cedula_usuario, rol_usuario):
         self.nombre_usuario = nombre_usuario
@@ -27,57 +26,116 @@ class usuarios(Base):
         self.rol_usuario = rol_usuario
 
 
-class logsUsuarios(Base):
-    __tablename__ = 'logsUsuarios'
+class logs_usuarios(Base):
+    __tablename__ = 'logs_usuarios'
     id = Column(Integer(), primary_key=True)
     fecha_login = Column(DateTime, nullable=False)
     usuarios_id = Column(Integer, ForeignKey('usuarios.id'))
-    usuarios = relationship("usuarios", back_populates="logsUsuarios")
+    usuarios = relationship("usuarios", back_populates="logs_usuarios")
 
     def __init__(self, fecha_login, usuarios_id):
         self.fecha_login = fecha_login
         self.usuarios_id = usuarios_id
 
 
-class CentroDeOperaciones(Base):
-    __tablename__ = 'CentroDeOperaciones'
+class centro_de_operaciones(Base):
+    __tablename__ = 'centro_de_operaciones'
     id = Column(Integer(), primary_key=True)
     nombre_centro_de_operaciones = Column(String(50), nullable=False)
-    division_id = (Integer, ForeignKey('Division.id'))
-    Division = relationship("Division", back_populates="CentroDeOperaciones")
-    Vehiculo = relationship("Vehiculo", back_populates = "CentroDeOperaciones")
+    division_id = Column(Integer, ForeignKey('division.id'))
+    division = relationship("division", back_populates="centro_de_operaciones")
+    vehiculo = relationship("vehiculo", back_populates="centro_de_operaciones")
 
     def __init__(self, nombre_centro_de_operaciones, division_id):
         self.nombre_centro_de_operaciones = nombre_centro_de_operaciones
         self.division_id = division_id
 
 
-class Division(Base):
-    __tablename__ = 'Division'
+class division(Base):
+    __tablename__ = 'division'
     id = Column(Integer(), primary_key=True)
     nombre_division = Column(String(50), nullable=False)
-    CentroDeOperaciones = relationship("CentroDeOperaciones", back_populates="Division")
+    centro_de_operaciones = relationship("centro_de_operaciones", back_populates="division")
 
     def __init__(self, nombre_division):
         self.nombre_division = nombre_division
 
 
-class Vehiculo(Base):
-    __tablena__ = 'Vehiculo'
+class vehiculo(Base):
+    __tablename__ = 'vehiculo'
     id = Column(Integer, primary_key=True)
     placa = Column(String(10), nullable=False, unique=True)
     modelo = Column(DateTime, nullable=False)
     capacidad = Column(Float, nullable=False)
-    centro_de_operaciones_id = Column(Integer, ForeignKey("CentroDeOperaciones.id"))
-    CentroDeOperaciones = relationship("Vehiculo", back_populates= "CentroDeOperaciones")
+    centro_de_operaciones_id = Column(Integer, ForeignKey("centro_de_operaciones.id"))
+    centro_de_operaciones = relationship("centro_de_operaciones", back_populates="vehiculo")
+    formato_preoperacional = relationship("formato_preoperacional", back_populates="vehiculo")
+    llantas = relationship("llantas", back_populates="vehiculo")
+    tanqueada = relationship("tanqueada", back_populates="vehiculo")
 
-    def __init__(self,placa,modelo,capacidad,centro_de_operaciones_id):
+    def __init__(self, placa, modelo, capacidad, centro_de_operaciones_id):
         self.placa = placa
         self.modelo = modelo
         self.capacidad = capacidad
         self.centro_de_operaciones_id = centro_de_operaciones_id
 
 
+class formato_preoperacional(Base):
+    __tablename__ = 'formato_preoperacional'
+    id = Column(Integer, primary_key=True)
+    fecha = Column(DateTime, nullable=False)
+    formulario = Column(JSON, nullable=False)
+    vehiculo_id = Column(Integer, ForeignKey("vehiculo.id"))
+    vehiculo = relationship("vehiculo", back_populates="formato_preoperacional")
+
+    def __init__(self, fecha, formulario, vehiculo_id):
+        self.fecha = fecha
+        self.formulario = formulario
+        self.vehiculo_id = vehiculo_id
 
 
+class llantas(Base):
+    __tablename__ = 'llantas'
+    id = Column(Integer, primary_key=True)
+    vehiculo_id = Column(Integer, ForeignKey("vehiculo.id"))
+    vehiculo = relationship("vehiculo", back_populates="llantas")
+    marca = Column(String, nullable=False)
+    posicion = Column(Integer, nullable=False)
+    profundidad = Column(Float, nullable=False)
+    kilometraje_instalacion = Column(Integer, nullable=False)
+    fecha_instalacion = Column(DateTime, nullable=False)
+    kilometraje_descarte = Column(Integer, nullable=True)
+    fecha_descarte = Column(DateTime, nullable=True)
+    motivo_descarte = Column(String, nullable=True)
+
+    def __init__(self, vehiculo_id, marca, posicion, profundidad, kilometraje_instalacion, fecha_instalacion,
+                 kilometraje_descarte, fecha_descarte, motivo_descarte):
+        self.vehiculo_id = vehiculo_id
+        self.marca = marca
+        self.posicion = posicion
+        self.profundidad = profundidad
+        self.kilometraje_instalacion = kilometraje_instalacion
+        self.kilometraje_descarte = kilometraje_descarte
+        self.fecha_descarte = fecha_descarte
+        self.motivo_descarte = motivo_descarte
+
+
+class tanqueada(Base):
+    __tablename__ = 'tanqueada'
+    id = Column(Integer, primary_key=True)
+    vehiculo_id = Column(Integer, ForeignKey("vehiculo.id"))
+    vehiculo = relationship("vehiculo", back_populates="tanqueada")
+    tanqueada_litros = Column(Float, nullable=False)
+    fecha_tanqueada = Column(DateTime, nullable=False)
+    costo_tanqueada = Column(Float, nullable=False)
+    kilometraje_tanqueada = Column(Integer, nullable=False)
+    combustible = Column(String, nullable=False)
+
+    def __init__(self, vehiculo_id,tanqueada_litros,fecha_tanqueada, costo_tanqueada,kilometraje_tanqueada,combustible):
+        self.vehiculo_id = vehiculo_id
+        self.tanqueada_litros = tanqueada_litros
+        self.fecha_tanqueada = fecha_tanqueada
+        self.costo_tanqueada = costo_tanqueada
+        self.kilometraje_tanqueada = kilometraje_tanqueada
+        self.combustible = combustible
 
